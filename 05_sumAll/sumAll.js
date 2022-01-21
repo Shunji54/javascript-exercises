e3d070ea14bbda9f36d7b5eabd1001d97978c15b
Shunji54 <kawalajason@gmail.com>
2022-01-21 15:46:34 +0100

    submit 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let x = 0;
+    if ( a < 0 || b < 0 || typeof a != 'number' || typeof b != 'number'  ) {
+        return "ERROR";
+    }
 
+    if (a < b && a > 0 && b > 0){
+        for(let i = a; i <= b; i++){
+            x += i;
+        }
+    } else {
+        for(let i = b; i <= a; i++){
+            x += i;
+        }
+    }
+    return x;
 };
 
 // Do not edit below this line
