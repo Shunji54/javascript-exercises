1f1b9b9fb98daa9d51bdc7c627c0939563fecc32
Shunji54 <kawalajason@gmail.com>
2022-01-21 16:23:17 +0100

    submit 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+    let temp = Math.round(((far-32) * (5/9)) * 10) / 10;
+    return temp
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let temp = Math.round(((cel*(9/5)) + 32)*10) / 10;
+  return temp
 };
 
 // Do not edit below this line
