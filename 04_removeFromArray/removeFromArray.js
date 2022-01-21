eaf9955f8f9949731ebe567025e25f432f4b281f
Shunji54 <kawalajason@gmail.com>
2022-01-21 15:10:52 +0100

    submit 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(arr,...values) {
+    let args = Array.prototype.slice.call(values);
+    for(let i = 0; i < args.length; i++) {
+        for(let j = 0; j < arr.length; j++) {
+            if(args[i] === arr[j]) {
+               arr.splice(j, 1);        
+            }
+        }
+    }
+    return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
