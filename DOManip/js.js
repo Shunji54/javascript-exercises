b252d96ef1ee0edde70487b94602092b2c9c6b97
Shunji54 <kawalajason@gmail.com>
2022-01-26 10:25:47 +0100

    deleted useless folder

---
+++ DOManip/js.js
@@ -0,0 +1,6 @@
+const pe = document.createElement('p');
+pe.classList.add('test');
+const test = document.querySelector('.test');
+
+test.textContent = "Hey I'm red!"
+document.body.appendChild(test);
