bc83b1f14f06dead178ba18ac508a0662adc2036
Jason <Shunji54@users.noreply.github.com>
2022-03-14 14:18:55 +0100

    09 exercice solution submission

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let re = /[\W_]/g;
+    let lowRegStr = str.toLowerCase().replace(re, '');
+    console.log(lowRegStr);
+    let reverseStr = lowRegStr.split('').reverse().join('');
+    console.log(reverseStr);
+    return reverseStr === lowRegStr;
 };
 
 // Do not edit below this line
