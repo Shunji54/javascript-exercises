218ffcace5fe07c2998b31b1d234a7b7f94d6a17
Jason <Shunji54@users.noreply.github.com>
2022-03-14 11:51:34 +0100

    exercice 08 solutions submission

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0
+  for (i=0;i<array.length;i++) {
+    total += array[i];
+  }
+  return Number(total);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1
+  for (i=0;i<array.length;i++) {
+    total *= array[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return Math.pow(x,y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let total = 1;
+	if (x==0 || x==1) {
+    return 1;
+  }
+  else {
+    for (i=1; i<x+1; i++) {
+      total *= i;
+    }
+  }
+  return total;
 };
 
 // Do not edit below this line
