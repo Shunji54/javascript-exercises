dd8e8f81bb7bdb4bcf68be11ff5e624e2662315c
Jason <Shunji54@users.noreply.github.com>
2022-03-14 14:50:29 +0100

    10 exercice solution submission

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (Number(number) < 0) {
+        return "OOPS";
+    } else if (Number(number) <= 2) {
+        return 1;
+    }
+    let first = 1;
+    let second = 1;
+    let sum = 0;
+    for (i = 2; i<Number(number) ;i++) {
+        sum = first+second;
+        first = second
+        second = sum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
